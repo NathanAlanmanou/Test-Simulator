1b6e342acf0e5418ecee5f1dac9770b9c66b71a0
NathanAlanmanou <nalanmanou@uchicago.edu>
2023-07-17 11:19:10 -0500

    commit

--- server/server.js
+++ server/server.js
@@ -17,14 +17,32 @@ const pool = new pg.Pool(pgConfig);
 
 // API route to save answers
 
+// app.post('http://localhost:5000/api/submit-test', (req, res) => {
+  
+//     const { email, answers, elapsedTime } = req.body;
+    
+//     pool.query(
+//       `INSERT INTO public."Test Entries" ("Entry ID", "Student Email", "Test ID", "Time Elapsed", "Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5") 
+//        VALUES (12345, $1, 1, $2, $3, $4, $5, $6, $7)`,
+//       [email, elapsedTime, answers[0], answers[1], answers[2], answers[3], answers[4]], 
+//       (err, result) => {
+//         if (err) {
+//           return res.status(500).send(err);
+//         }
+//         res.sendStatus(200);
+//       }
+//     );
+  
+//   });
+
 app.post('http://localhost:5000/api/submit-test', (req, res) => {
   
-    const { email, answers, elapsedTime } = req.body;
+    const { email, elapsedTime } = req.body;
     
     pool.query(
       `INSERT INTO public."Test Entries" ("Entry ID", "Student Email", "Test ID", "Time Elapsed", "Answer 1", "Answer 2", "Answer 3", "Answer 4", "Answer 5") 
-       VALUES (12345, $1, 1, $2, $3, $4, $5, $6, $7)`,
-      [email, elapsedTime, answers[0], answers[1], answers[2], answers[3], answers[4]], 
+       VALUES (12345, $1, 1, $2, 1, 2, 3, 4, 5)`,
+      [email, elapsedTime], 
       (err, result) => {
         if (err) {
           return res.status(500).send(err);
